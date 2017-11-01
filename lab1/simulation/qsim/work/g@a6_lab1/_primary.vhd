c1bdbea75b565e29ae324771174c29fd4ea20fd2
Abbas <abbas_y@hotmail.ca>
2017-11-01 16:24:42 -0400

    Lab3 update

--- lab1/simulation/qsim/work/g@a6_lab1/_primary.vhd
+++ lab1/simulation/qsim/work/g@a6_lab1/_primary.vhd
@@ -5,7 +5,8 @@ entity gA6_lab1 is
         AeqB            : out    vl_logic;
         B               : in     vl_logic_vector(6 downto 0);
         C               : in     vl_logic_vector(6 downto 0);
-        Amod13          : out    vl_logic_vector(3 downto 0);
-        A               : in     vl_logic_vector(5 downto 0)
+        Afloor13        : out    vl_logic_vector(2 downto 0);
+        A               : in     vl_logic_vector(5 downto 0);
+        Amod13          : out    vl_logic_vector(3 downto 0)
     );
 end gA6_lab1;
--- lab1/simulation/qsim/work/g@a6_lab1_vlg_check_tst/_primary.vhd
+++ lab1/simulation/qsim/work/g@a6_lab1_vlg_check_tst/_primary.vhd
@@ -3,6 +3,7 @@ use verilog.vl_types.all;
 entity gA6_lab1_vlg_check_tst is
     port(
         AeqB            : in     vl_logic;
+        Afloor13        : in     vl_logic_vector(2 downto 0);
         Amod13          : in     vl_logic_vector(3 downto 0);
         sampler_rx      : in     vl_logic
     );
