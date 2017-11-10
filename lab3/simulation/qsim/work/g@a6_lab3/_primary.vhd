88b0a3be3b015ac00f48c9a746b60e423d1b5743
Abbas <abbas_y@hotmail.ca>
2017-11-09 23:50:22 -0500

    Done Lab3

--- lab3/simulation/qsim/work/g@a6_lab3/_primary.vhd
+++ lab3/simulation/qsim/work/g@a6_lab3/_primary.vhd
@@ -2,18 +2,22 @@ library verilog;
 use verilog.vl_types.all;
 entity gA6_lab3 is
     port(
+        altera_reserved_tms: in     vl_logic;
+        altera_reserved_tck: in     vl_logic;
+        altera_reserved_tdi: in     vl_logic;
+        altera_reserved_tdo: out    vl_logic;
         empty           : out    vl_logic;
-        enable          : in     vl_logic;
+        button          : in     vl_logic;
         rst             : in     vl_logic;
         clk             : in     vl_logic;
         addr            : in     vl_logic_vector(5 downto 0);
-        data            : in     vl_logic_vector(5 downto 0);
         mode            : in     vl_logic_vector(1 downto 0);
         full            : out    vl_logic;
+        led_empty       : out    vl_logic_vector(6 downto 0);
         led_floor       : out    vl_logic_vector(6 downto 0);
-        button          : in     vl_logic;
-        mode_segment    : in     vl_logic;
+        led_lights      : out    vl_logic_vector(9 downto 0);
         led_mod         : out    vl_logic_vector(6 downto 0);
+        led_mode        : out    vl_logic_vector(6 downto 0);
         num             : out    vl_logic_vector(5 downto 0);
         value           : out    vl_logic_vector(5 downto 0)
     );
--- lab3/simulation/qsim/work/g@a6_lab3_vlg_check_tst/_primary.vhd
+++ lab3/simulation/qsim/work/g@a6_lab3_vlg_check_tst/_primary.vhd
@@ -4,8 +4,11 @@ entity gA6_lab3_vlg_check_tst is
     port(
         empty           : in     vl_logic;
         full            : in     vl_logic;
+        led_empty       : in     vl_logic_vector(6 downto 0);
         led_floor       : in     vl_logic_vector(6 downto 0);
+        led_lights      : in     vl_logic_vector(9 downto 0);
         led_mod         : in     vl_logic_vector(6 downto 0);
+        led_mode        : in     vl_logic_vector(6 downto 0);
         num             : in     vl_logic_vector(5 downto 0);
         value           : in     vl_logic_vector(5 downto 0);
         sampler_rx      : in     vl_logic
