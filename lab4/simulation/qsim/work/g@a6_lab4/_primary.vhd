bd3d5a535890495cedf42970dc1b8e902489d39a
Abbas <abbas_y@hotmail.ca>
2017-11-24 23:55:12 -0500

    Lab4 done

--- lab4/simulation/qsim/work/g@a6_lab4/_primary.vhd
+++ lab4/simulation/qsim/work/g@a6_lab4/_primary.vhd
@@ -15,11 +15,9 @@ entity gA6_lab4 is
         mode            : in     vl_logic_vector(1 downto 0);
         full            : out    vl_logic;
         legal_play      : out    vl_logic;
+        ace_out         : out    vl_logic;
         hand_sum        : in     vl_logic_vector(5 downto 0);
         new_card        : in     vl_logic_vector(5 downto 0);
-        ace_out         : out    vl_logic;
-        out_4           : out    vl_logic;
-        out_5           : out    vl_logic;
         rand_enable     : out    vl_logic;
         legal_num       : in     vl_logic;
         stack_enable    : out    vl_logic;
@@ -28,9 +26,6 @@ entity gA6_lab4 is
         led_mode        : out    vl_logic_vector(6 downto 0);
         led_suit        : out    vl_logic_vector(6 downto 0);
         led_value       : out    vl_logic_vector(6 downto 0);
-        num             : out    vl_logic_vector(5 downto 0);
-        out_1           : out    vl_logic_vector(5 downto 0);
-        out_2           : out    vl_logic_vector(5 downto 0);
-        out_3           : out    vl_logic_vector(5 downto 0)
+        num             : out    vl_logic_vector(5 downto 0)
     );
 end gA6_lab4;
--- lab4/simulation/qsim/work/g@a6_lab4_vlg_check_tst/_primary.vhd
+++ lab4/simulation/qsim/work/g@a6_lab4_vlg_check_tst/_primary.vhd
@@ -12,11 +12,6 @@ entity gA6_lab4_vlg_check_tst is
         led_value       : in     vl_logic_vector(6 downto 0);
         legal_play      : in     vl_logic;
         num             : in     vl_logic_vector(5 downto 0);
-        out_1           : in     vl_logic_vector(5 downto 0);
-        out_2           : in     vl_logic_vector(5 downto 0);
-        out_3           : in     vl_logic_vector(5 downto 0);
-        out_4           : in     vl_logic;
-        out_5           : in     vl_logic;
         rand_enable     : in     vl_logic;
         stack_enable    : in     vl_logic;
         sampler_rx      : in     vl_logic
